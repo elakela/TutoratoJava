d476c28f26ef73272b5139b9df4b74832a6a83e9
Gabriela Riscica <104057809+elakela@users.noreply.github.com>
2023-05-06 14:54:32 +0000

    lettore di parole

--- WordCounter.java
+++ WordCounter.java
@@ -8,17 +8,17 @@ import java.io.PrintWriter;
 public class WordCounter{
     
     // questo primo metodo serve per leggere il numero di parole in un file, viene passato il file di input
-    private static int readWords(String file){
-        try(FileReader fileReader = new FileReader(file)){ // in questo modo viene istanziato un oggetto con cui si può leggere da file 
-            BufferedReader bufferReader = new BufferedReader(fileReader); // in questo modo viene istanziato un oggetto con cui si può leggere da buffer, in modo da leggere in maniera più efficiente
+    private static int readWords(String file) {
+        try (FileReader fileReader = new FileReader(file)){ // in questo modo viene istanziato un oggetto con cui si può leggere da file 
+            BufferedReader bufferedReader = new BufferedReader(fileReader); // in questo modo viene istanziato un oggetto con cui si può leggere da buffer, in modo da leggere in maniera più efficiente
             String line; 
             int wordCount = 0;
 
-            while((line = bufferReader.readLine())!= null){ // esiste quindi un metodo di default per il bufferReader che consente la lettura di una stringa
+            while((line = bufferedReader.readLine())!= null){ // esiste quindi un metodo di default per il bufferedReader che consente la lettura di una stringa
                 String[] words = line.split(" "); // creare un array di stringhe il cui delimitatore tra una parola e un'altra e uno spazio il che rende possibile contare le stringhe
                 wordCount+=words.length;
             }
-                bufferReader.close();
+                bufferedReader.close();
                 return wordCount;
         }       catch(IOException e){ 
             e.printStackTrace(); //spiega l'eccezione e dove è avvenuta l'eccezione
@@ -39,16 +39,18 @@ public class WordCounter{
             System.exit(1);
          }
     }
-
-    public static void main(String[] args){
-        if(args.length != 2){ 
+  
+    public static void main(String[] args) {
+        if (args.length != 2) {
             System.err.println("Usage: java WordCounter <input_file> <output_file>");
             System.exit(1);
         }
-
+ 
         String inputFile = args[0];
         String outputFile = args[1];
         int wordCount = readWords(inputFile);
         writeCount(outputFile, wordCount);
     }
+    
 }
+
