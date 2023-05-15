c42be7e5cbf8aaf44d917624e54e5f48c94bbb98
Gabriela Riscica <104057809+elakela@users.noreply.github.com>
2023-05-15 10:00:39 +0000

    Hello written in red

--- EserciziTutor/HelloWorld.java
+++ EserciziTutor/HelloWorld.java
@@ -4,3 +4,32 @@ public class HelloWorld{
         System.out.println("HelloWorld");
     }
 }
+
+
+
+/*
+ * 
+ * 
+private static void wait(int n){
+    try{Thread.sleep(n);}catch(InterruptedException e){System.out.println(e);}
+}
+private static void Print(){
+    System.out.println("Hello World");
+    wait(2000);
+    System.out.println("\n\n .");
+    wait(1000);
+    System.out.println(".");
+    wait(1000);
+    System.out.println(".");
+    wait(4000);
+    try{Thread.sleep(4000);}catch(InterruptedException e){System.out.println(e);}
+    System.out.println()
+    try{Thread.sleep(4000);}catch(InterruptedException e){System.out.println(e);}
+    System.out.print("\033[H\033[2J");
+    //System.out.println(ANSI_ITALICS, "-hello there");
+    System.out.println("\n-General Kenobi");
+}
+
+ * 
+ * 
+ */
--- HelloThere.java
+++ HelloThere.java
@@ -1,9 +1,11 @@
-public class HelloThere extends Thread {
+public class HelloThere{
     private static void Print(){
+        System.out.print("\u001B[31m");
         System.out.println("Hello World");
-        System.out.println("\n\n...");
+        System.out.print("\u001B[2J");
+
+        System.out.println("...");
     }
-    
     public static void main(String[] args){
         Print();
     }    
